c3919acab7a855ef3639a8d1666ba04af901cb3d
Alexandr <alex_buryakov@mail.ru>
2022-01-25 21:00:24 +0300

    Lession 3 by android. Append course card.

--- app/src/main/java/mirea/buryakov/e_commerece/MainActivity.java
+++ app/src/main/java/mirea/buryakov/e_commerece/MainActivity.java
@@ -10,12 +10,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import mirea.buryakov.e_commerece.adapter.CatagoryAdapter;
+import mirea.buryakov.e_commerece.adapter.CourseAdapter;
 import mirea.buryakov.e_commerece.model.Category;
+import mirea.buryakov.e_commerece.model.Course;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView categoryRecycler;
+    RecyclerView categoryRecycler, courseRecycler;
     CatagoryAdapter catagoryAdapter;
+    CourseAdapter courseAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(4, "Прочее"));
 
         setCategoryRecycler(categoryList);
+
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1, "java","Профессия Java\nразработчик","1 янв","начальный","#424345"));
+        courseList.add(new Course(2, "python","Профессия python\nразработчик","10 янв","начальный","#9FA52D"));
+
+        setCourseRecycler(courseList);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,RecyclerView.HORIZONTAL,false);
+
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this, courseList);
+        courseRecycler.setAdapter(courseAdapter);
     }
 
     private void setCategoryRecycler(List<Category> categoryList) {
